2f3a73fe0da4d95c178fe05caa01c21c36e8a27b
Mylo Faulkner <mylofaulkner27@gmail.com>
2025-07-19 19:46:26 +0100

    most of the logic for logging in and registering

--- client/src/components/LoginRegisterForm.tsx
+++ client/src/components/LoginRegisterForm.tsx
@@ -1,5 +1,8 @@
+import { useMutation } from "@tanstack/react-query";
 import { useState } from "react";
-import { Link } from "react-router";
+import { Link, useNavigate } from "react-router";
+import Spinner from "./Spinner";
+import axios, { AxiosError } from "axios";
 
 interface FormData {
     username: string;
@@ -11,45 +14,93 @@ interface LoginRegisterFormProps {
 }
 
 const LoginRegisterForm = ({ formType }: LoginRegisterFormProps) => {
+    const navigate = useNavigate();
+    const mutation = useMutation({
+        mutationFn: async (formData: FormData) => {
+            setFormData({ username: "", password: "" });
+            await axios.post(`/api/auth/${formType.toLowerCase()}`, formData);
+        },
+    });
+
     const [formData, setFormData] = useState<FormData>({
         username: "",
         password: "",
     });
 
+    if (mutation.isSuccess) {
+        navigate("/dashboard");
+    }
+
     const inputClass: string =
-        "px-5 py-3 bg-gray-800 rounded w-full focus:outline-none focus:ring-2 focus:ring-blue-500";
+        "px-5 py-3 bg-gray-800 rounded w-full focus:outline-none focus:ring-2 focus:ring-blue-500 disabled:bg-gray-700";
     const buttonClass: string =
-        "px-5 py-3 bg-blue-500 hover:bg-blue-700 rounded transition-colors cursor-pointer w-full";
+        "px-5 py-3 bg-blue-500 hover:bg-blue-700 rounded transition-colors cursor-pointer w-full disabled:bg-gray-500 disabled:cursor-default";
+
+    const usernameInput = (
+        <input
+            type="text"
+            className={inputClass}
+            placeholder="username..."
+            value={formData.username}
+            onChange={(e) =>
+                setFormData({
+                    ...formData,
+                    username: e.target.value,
+                })
+            }
+            disabled={mutation.isPending}
+        />
+    );
+
+    const passwordInput = (
+        <input
+            type="password"
+            className={inputClass}
+            placeholder="password..."
+            value={formData.password}
+            onChange={(e) =>
+                setFormData({
+                    ...formData,
+                    password: e.target.value,
+                })
+            }
+            disabled={mutation.isPending}
+        />
+    );
+
+    const submitButton = (
+        <button
+            type="submit"
+            className={buttonClass}
+            onClick={() => mutation.mutate(formData)}
+            disabled={mutation.isPending}
+        >
+            {!mutation.isPending ? (
+                formType
+            ) : (
+                <span className="flex items-center justify-center">
+                    <Spinner />
+                </span>
+            )}
+        </button>
+    );
 
     return (
         <div className="flex items-center justify-center h-screen w-full">
             <div className="flex flex-col gap-4 items-center justify-center h-screen w-[40%] text-white text-2xl">
                 <span className="mb-10 text-3xl">{formType}</span>
-                <input
-                    className={inputClass}
-                    placeholder="username..."
-                    value={formData.username}
-                    onChange={(e) =>
-                        setFormData({ ...formData, username: e.target.value })
-                    }
-                />
-                <input
-                    className={inputClass}
-                    placeholder="password..."
-                    value={formData.password}
-                    onChange={(e) =>
-                        setFormData({ ...formData, password: e.target.value })
-                    }
-                />
-                <button
-                    type="submit"
-                    className={buttonClass}
-                    onClick={() => {
-                        console.log("Form submitted:", formData);
-                    }}
-                >
-                    {formType}
-                </button>
+                {usernameInput}
+                {passwordInput}
+
+                {mutation.isError && (
+                    <span className="text-red-500 text-lg self-start">
+                        {mutation.error instanceof AxiosError
+                            ? mutation.error.response?.data?.detail
+                            : "Internal server error"}
+                    </span>
+                )}
+
+                {submitButton}
                 <Link className={buttonClass + " text-center"} to={"/"}>
                     Home
                 </Link>
