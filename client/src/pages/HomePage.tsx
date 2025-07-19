ed9809eae21afb32b2fd113f4129fce07c4ef1a4
Mylo Faulkner <mylofaulkner27@gmail.com>
2025-07-19 18:39:13 +0100

    change font of title

--- client/src/pages/HomePage.tsx
+++ client/src/pages/HomePage.tsx
@@ -2,11 +2,13 @@ import { Link } from "react-router";
 
 const HomePage = () => {
     const linkClass: string =
-        "w-[20%] text-center px-5 py-3 bg-blue-500 hover:bg-blue-700 rounded transition-colors cursor-pointer";
+        "w-[40%] text-center px-5 py-3 bg-blue-500 hover:bg-blue-700 rounded transition-colors cursor-pointer";
 
     return (
         <div className="flex flex-col gap-4 items-center justify-center h-screen w-full text-white text-2xl">
-            <span className="mb-10">Access and Refresh Token Demo</span>
+            <span className="mb-10 text-3xl">
+                Access and Refresh Token Demo
+            </span>
             <Link className={linkClass} to={"/login"}>
                 Login
             </Link>
