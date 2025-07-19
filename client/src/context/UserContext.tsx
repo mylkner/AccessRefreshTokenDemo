89e4728d08663816b105c8dc3370667a9c056d61
Mylo Faulkner <mylofaulkner27@gmail.com>
2025-07-19 20:17:50 +0100

    added types

--- client/src/context/UserContext.tsx
+++ client/src/context/UserContext.tsx
@@ -2,14 +2,17 @@ import { createContext, useContext, useState, type Dispatch } from "react";
 
 interface AuthContextProps {
     accessToken: string | null;
-    setAccessToken: Dispatch<React.SetStateAction<null>>;
+    setAccessToken: Dispatch<React.SetStateAction<null>> | null;
 }
 
 interface AuthProviderProps {
     children: React.ReactNode;
 }
 
-const AuthContext = createContext<AuthContextProps | null>(null);
+const AuthContext = createContext<AuthContextProps>({
+    accessToken: null,
+    setAccessToken: null,
+});
 
 export const AuthProvider = ({ children }: AuthProviderProps) => {
     const [accessToken, setAccessToken] = useState(null);
