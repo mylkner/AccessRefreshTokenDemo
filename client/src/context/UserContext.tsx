cb773e4d496359bc89efd0c444fbad31ef481dc4
Mylo Faulkner <mylofaulkner27@gmail.com>
2025-07-19 20:30:11 +0100

    check for null inside component

--- client/src/context/UserContext.tsx
+++ client/src/context/UserContext.tsx
@@ -2,17 +2,14 @@ import { createContext, useContext, useState, type Dispatch } from "react";
 
 interface AuthContextProps {
     accessToken: string | null;
-    setAccessToken: Dispatch<React.SetStateAction<string | null>> | null;
+    setAccessToken: Dispatch<React.SetStateAction<string | null>>;
 }
 
 interface AuthProviderProps {
     children: React.ReactNode;
 }
 
-const AuthContext = createContext<AuthContextProps>({
-    accessToken: null,
-    setAccessToken: null,
-});
+const AuthContext = createContext<AuthContextProps | null>(null);
 
 export const AuthProvider = ({ children }: AuthProviderProps) => {
     const [accessToken, setAccessToken] = useState<string | null>(null);
@@ -24,4 +21,9 @@ export const AuthProvider = ({ children }: AuthProviderProps) => {
     );
 };
 
-export const useAuth = () => useContext(AuthContext);
+export const useAuth = () => {
+    const context = useContext(AuthContext);
+    if (context === null)
+        throw new Error("AuthContext must be used within an AuthProvider");
+    return context;
+};
