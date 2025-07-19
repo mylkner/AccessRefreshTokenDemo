3dead75f59215d711cb88a69bb3e6223afc0ad44
Mylo Faulkner <mylofaulkner27@gmail.com>
2025-07-19 21:27:10 +0100

    added public and auth layouts

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -2,20 +2,22 @@ import { Routes, Route, BrowserRouter } from "react-router";
 import HomePage from "./pages/HomePage";
 import Login from "./pages/Login";
 import Register from "./pages/Register";
-import AuthLayout from "./routes/AuthLayout";
 import AdminPage from "./pages/AdminPage";
 import NonAdminPage from "./pages/NonAdminPage";
 import AdminLayout from "./routes/AdminLayout";
 import Dashboard from "./pages/Dashboard";
+import AuthLayout from "./routes/AuthLayout";
+import PublicLayout from "./routes/PublicLayout";
 
 function App() {
     return (
         <BrowserRouter>
             <Routes>
-                <Route path="/" element={<HomePage />} />
-                <Route path="/login" element={<Login />} />
-                <Route path="/register" element={<Register />} />
-
+                <Route element={<PublicLayout />}>
+                    <Route path="/" element={<HomePage />} />
+                    <Route path="/login" element={<Login />} />
+                    <Route path="/register" element={<Register />} />
+                </Route>
                 <Route element={<AuthLayout />}>
                     <Route path="/dashboard" element={<Dashboard />} />
                     <Route path="/non-admin" element={<NonAdminPage />} />
