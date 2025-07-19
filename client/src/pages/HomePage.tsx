9d4d5526ae89d3ccc2546499bb82474049d1814d
Mylo Faulkner <mylofaulkner27@gmail.com>
2025-07-19 18:20:13 +0100

    finish home page

--- client/src/pages/HomePage.tsx
+++ client/src/pages/HomePage.tsx
@@ -1,5 +1,20 @@
+import { Link } from "react-router";
+
 const HomePage = () => {
-    return <div>HomePage</div>;
+    const linkClass: string =
+        "w-[20%] text-center px-5 py-3 bg-blue-500 hover:bg-blue-700 rounded transition-colors cursor-pointer";
+
+    return (
+        <div className="flex flex-col gap-4 items-center justify-center h-screen w-full text-white text-2xl">
+            <span className="mb-10">Access and Refresh Token Demo</span>
+            <Link className={linkClass} to={"/login"}>
+                Login
+            </Link>
+            <Link className={linkClass} to={"/register"}>
+                Register
+            </Link>
+        </div>
+    );
 };
 
 export default HomePage;
