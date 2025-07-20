4ee293442aaa66daa71fbc48f60cd4b1d2cc7f9a
Mylo Faulkner <mylofaulkner27@gmail.com>
2025-07-20 21:49:29 +0100

    added token provider to set access token on page refresh

---
+++ client/src/components/TokenProvider.tsx
@@ -0,0 +1,34 @@
+import { type ReactNode } from "react";
+import { useAuthStore } from "../store/zustand";
+import { useQuery } from "@tanstack/react-query";
+import axios from "axios";
+import Spinner from "./Spinner";
+
+interface TokenProviderProps {
+    children: ReactNode;
+}
+
+const TokenProvider = ({ children }: TokenProviderProps) => {
+    const { setAccessToken } = useAuthStore((state) => state);
+
+    const { isLoading } = useQuery({
+        queryKey: ["accessToken"],
+        queryFn: async () => {
+            const { data } = await axios.get("/api/auth/refresh-token");
+            setAccessToken(data);
+        },
+        retry: false,
+        refetchOnWindowFocus: false,
+    });
+
+    if (isLoading)
+        return (
+            <div className="flex items-center justify-center h-screen">
+                <Spinner />
+            </div>
+        );
+
+    return <div>{children}</div>;
+};
+
+export default TokenProvider;
--- client/src/main.tsx
+++ client/src/main.tsx
@@ -1,12 +1,15 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import TokenProvider from "./components/TokenProvider.tsx";
 import App from "./App.tsx";
 
 createRoot(document.getElementById("root")!).render(
     <StrictMode>
         <QueryClientProvider client={new QueryClient()}>
-            <App />
+            <TokenProvider>
+                <App />
+            </TokenProvider>
         </QueryClientProvider>
     </StrictMode>
 );
