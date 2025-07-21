bb1676113464485e8fe29a0c2dce35ff51fbff2a
Mylo Faulkner <mylofaulkner27@gmail.com>
2025-07-21 01:27:17 +0100

    remove public

--- Server/Server/Services/Interfaces/IAuthService.cs
+++ Server/Server/Services/Interfaces/IAuthService.cs
@@ -5,10 +5,10 @@ namespace Server.Services.Interfaces;
 
 public interface IAuthService
 {
-    public Task RegisterAsync(UserDto req);
-    public Task<string> LoginAsync(UserDto req, HttpContext context);
-    public Task LogoutAsync(HttpContext context);
-    public Task DeleteAsync(HttpContext context);
-    public Task<string> ValidateAndReplaceRefreshTokenAsync(HttpContext context);
-    public Task<string> ChangeRole(HttpContext context);
+    Task RegisterAsync(UserDto req);
+    Task<string> LoginAsync(UserDto req, HttpContext context);
+    Task LogoutAsync(HttpContext context);
+    Task DeleteAsync(HttpContext context);
+    Task<string> ValidateAndReplaceRefreshTokenAsync(HttpContext context);
+    Task<string> ChangeRole(HttpContext context);
 }
